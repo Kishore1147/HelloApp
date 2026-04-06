daddde2f0f772f38f21f40f6e040b26a577eb7b5
Kishore114 <ka6311@srmist.edu.in>
2026-04-06 14:01:39 +0530

    UC2 implementation

--- eclipse-workspace/HelloApp/src/main/java/com/example/HelloApp.java
+++ eclipse-workspace/HelloApp/src/main/java/com/example/HelloApp.java
@@ -2,6 +2,12 @@ package com.example;
 
 public class HelloApp {
 	public static void main (String[] args) {
-		System.out.println("Hello, World");
+		 if (args.length > 0) {
+	            String name = args[0];
+	            System.out.println("Hello, " + name + "!");
+	        } else {
+	            // Default case (same as UC1)
+	            System.out.println("Hello, World!");
+	        }
 	}
 }
