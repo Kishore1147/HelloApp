a34eb06e7e82cc73ac53d1580f34034ec7f6d83d
Kishore114 <ka6311@srmist.edu.in>
2026-04-10 13:15:18 +0530

    UC4 implementation

--- eclipse-workspace/HelloApp/src/main/java/com/example/HelloApp.java
+++ eclipse-workspace/HelloApp/src/main/java/com/example/HelloApp.java
@@ -2,12 +2,12 @@ package com.example;
 
 public class HelloApp {
 	public static void main (String[] args) {
-		 if (args.length > 0) {
-	            String name = args[0];
-	            System.out.println("Hello, " + name + "!");
-	        } else {
-	            // Default case (same as UC1)
-	            System.out.println("Hello, World!");
-	        }
+		String names;
+        if (args.length == 0) {
+            names = "World";
+        } else {
+            names = String.join(", ", args);
+        }
+        System.out.println("Hello, " + names + "!");
 	}
 }
