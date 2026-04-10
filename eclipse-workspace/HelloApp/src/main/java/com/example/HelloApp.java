fcf69c6e1cd631cfebcacc838d551f05632a0d85
Kishore114 <ka6311@srmist.edu.in>
2026-04-10 13:51:46 +0530

    UC5 implementation

--- eclipse-workspace/HelloApp/src/main/java/com/example/HelloApp.java
+++ eclipse-workspace/HelloApp/src/main/java/com/example/HelloApp.java
@@ -2,10 +2,22 @@ package com.example;
 
 public class HelloApp {
 	public static void main (String[] args) {
-		String name = "World";
-        if (args.length > 0) {
-            name = args[0];
+		String names;
+       if (args.length == 0) {
+            names = "World";
+        } else {
+            StringBuilder nameBuilder = new StringBuilder();
+            boolean first = true;            
+            for (String name : args) {
+                if (!first) {
+                    nameBuilder.append(", ");
+                }
+                nameBuilder.append(name);
+                first = false;
+            }
+
+            names = nameBuilder.toString();
         }
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Hello, " + names + "!");
 	}
 }
