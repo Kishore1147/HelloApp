4d0a8d1617164f7a5a4407550f715cd1cda0720e
Kishore114 <ka6311@srmist.edu.in>
2026-04-10 12:42:05 +0530

    UC3 Implimentation

--- eclipse-workspace/HelloApp/src/main/java/com/example/HelloApp.java
+++ eclipse-workspace/HelloApp/src/main/java/com/example/HelloApp.java
@@ -2,12 +2,10 @@ package com.example;
 
 public class HelloApp {
 	public static void main (String[] args) {
-		 if (args.length > 0) {
-	            String name = args[0];
-	            System.out.println("Hello, " + name + "!");
-	        } else {
-	            // Default case (same as UC1)
-	            System.out.println("Hello, World!");
-	        }
+		String name = "World";
+        if (args.length > 0) {
+            name = args[0];
+        }
+        System.out.println("Hello, " + name + "!");
 	}
 }
